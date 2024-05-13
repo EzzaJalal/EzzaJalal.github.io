42774f12f7a27b42fa69d1f4038516b1be2deac1
EzzaJalal <159177985+EzzaJalal@users.noreply.github.com>
2024-05-13 13:29:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ //Using Javascript for toggle menu in mobile phones
+ 
+ var navLinks = document.getElementById("navLinks");
+   
+    function showMenu(){
+        navLinks.style.display= "block";
+    }
+     function hideMenu(){
+        navLinks.style.display= "none";
+    }
